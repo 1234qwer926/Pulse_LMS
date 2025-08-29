dc02329ce64bbac629647b5e22635327723c05c8
1234qwer926 <srao50872@gmail.com>
2025-08-29 14:02:30 +0530

    theme changes

--- src/main.jsx
+++ src/main.jsx
@@ -11,7 +11,7 @@ import '@mantine/notifications/styles.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <MantineProvider forceColorScheme="dark">
+    <MantineProvider >
       <ModalsProvider>
         <Notifications />
         <App />
